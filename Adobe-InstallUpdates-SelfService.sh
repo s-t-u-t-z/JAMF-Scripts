9a6f14fec99e5498d77cb8c9c8cf540392c5990a
stuutz <32516714+stuutz@users.noreply.github.com>
2019-02-14 10:32:14 -0500

    Update Adobe-InstallUpdates-SelfService.sh

--- Adobe-InstallUpdates-SelfService.sh
+++ Adobe-InstallUpdates-SelfService.sh
@@ -19,6 +19,7 @@
 #	- User will be shown which app is being downloaded/updated during the process
 #	- When finished the user will be prompted that updates have completed
 #	- If there are no updates available it will prompt the user
+#	- The script will fail if no user is signed in (this is on purpose)
 #
 #	Notes:
 #	- Package all Adobe app icon (.icns) files into a folder called "AdobeIcons" and install
