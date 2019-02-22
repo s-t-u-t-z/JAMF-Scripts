13347d9818697555f9f504d185a8ae66a263b30c
stuutz <32516714+stuutz@users.noreply.github.com>
2019-02-22 10:09:01 -0500

    Update JAMFMessage-PIDkill.sh

--- JAMFMessage-PIDkill.sh
+++ JAMFMessage-PIDkill.sh
@@ -9,26 +9,26 @@
 #
 # DESCRIPTION
 #
-#	This script utilizes the jamfHelper window to inform users before a policy runs.  The parameters
-#	in the script policy control what is said in the message.  The script also has a built in app
-#	running checker to make sure the app quits before the app updates.
+#   This script utilizes the jamfHelper window to inform users before a policy runs.  The parameters
+#   in the script policy control what is said in the message.  The script also has a built in app
+#   running checker to make sure the app quits before the app updates.
 #
-# 	Parameters for jamfHelper window:
-# 	$4	= Window Type (hud, utility, fs)
-# 	$5	= Window Title
-# 	$6	= Window Heading
-# 	$7	= Window Description
-# 	$8	= Button Text
-# 	$9	= Kill app process
-# 	$10	= Icon Path (ex: /Applications/Self Service.app/Contents/Resources/AppIcon.icns)
+#  Parameters for jamfHelper window:
+#   $4	= Window Type (hud, utility, fs)
+#   $5	= Window Title
+#   $6	= Window Heading
+#   $7	= Window Description
+#   $8	= Button Text
+#   $9	= Kill app process
+#   $10	= Icon Path (ex: /Applications/Self Service.app/Contents/Resources/AppIcon.icns)
 #
 # VERSION
 #
-#	- 1.0
+#   - 1.0
 #
 # CHANGE HISTORY
 #
-# 	- Created script - 2/18/19 (1.0)
+#   - Created script - 2/18/19 (1.0)
 #
 ####################################################################################################
 
