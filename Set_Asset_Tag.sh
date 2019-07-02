d5146048ec01da7c7048e00d510d16492f44b8ac
stuutz <32516714+stuutz@users.noreply.github.com>
2019-07-02 07:46:15 -0400

    Update Set_Asset_Tag.sh

--- Set_Asset_Tag.sh
+++ Set_Asset_Tag.sh
@@ -16,38 +16,55 @@
 #
 # VERSION
 #
-# - 1.0
+# - 1.1
 #
 # CHANGE HISTORY
 #
 # - Created script - 4/22/19 (1.0)
+# - Modified the script wording to make better sense - 7/2/19 (1.1)
 #
 ####################################################################################################
 
 
+##################################################################
+## Script variables
+##################################################################
+
 # Get computer name
-compNAME=`hostname`
+compName=`hostname`
 
 # Dialog box to ask for asset tag
-assetTAG="$(osascript -e 'Tell application "System Events" to display dialog "Enter the Asset Tag:" default answer "" with text buttons {"OK"} default button 1' -e 'text returned of result')"
+assetTag="$(osascript -e 'Tell application "System Events" to display dialog "Enter the Asset Tag:" default answer "" with text buttons {"OK"} default button 1' -e 'text returned of result')"
+
+
+##################################################################
+## Main script
+##################################################################
 
 # Dialog box to confirm asset tag
 osascript >/dev/null 2>&1 <<-EOF
 Tell application "System Events"
-set Msg1 to "Does this information look correct?" & return & return & "Asset Tag: ${assetTAG}" & return & "Computer: ${compNAME}"
-set Res1 to display dialog Msg1 buttons {"Cancel", "OK"} default button 1
+set Msg1 to "Does this information look correct?" & return & return & "Asset Tag: ${assetTag}" & return & "Computer: ${compName}"
+set Res1 to display dialog Msg1 buttons {"Cancel", "YES"} default button 1
 end tell
 EOF
 
 # If CANCEL button is clicked
 if [ "$?" != "0" ] ; then
-  echo "User canceled. Exiting..."
-  exit 0
+  
+	echo "User canceled. Exiting..."
+  
+	exit 0
+    
 else
-  # If OK button is clicked
-  echo "Applying asset tag ${assetTAG} to computer ${compNAME}"
-  jamf recon -assetTag $assetTAG
+
+	# If OK button is clicked
+	echo "Applying asset tag ${assetTag} to computer ${compName}"
+	
+  jamf recon -assetTag $assetTag
+    
 fi
 
 
 exit 0
+
